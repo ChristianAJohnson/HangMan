e1d427dff7df4a7481b9d8749cf25a2b69e7d0a8
christianajohnson <chrisgochris@gmail.com>
2018-05-12 20:49:00 -0400

    hint is now showing in the top div, and hidden letters are  showing up when clicking on correct letters, and *'s still stay for letters that havent been chosen

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
+var guessText = document.createElement("div")
+var wrapper = document.getElementsByClassName('wrapper')[0];
+var A = document.getElementsByClassName("A")[0];
+var B = document.getElementsByClassName("B")[0];
 
 function createletterboxes(word,hintphrase)
 {	
 
-	var guessWord = word.replace(/[a-z]/gi, "*"); // makes a variable called guest word which contains the word, but is replace with *
+	var guessWord = word.replace(/[a-z]/gi, "*"); // makes a variable called guestWord which contains the word, but is replaced with *
 	var guessWord2 = guessWord.split("")
 	var word2 = word.toUpperCase(); // turns string into upper case
+
 	word2 = word2.split(""); // splits up string into an array
-	var wrapper = document.createElement('div')
+
+
+	// var wrapper = document.createElement('div') // this creates a wrapper
+	// var wrapper = document.getElementsByClassName('wrapper')[0]; // this creates a wrapper
 	wrapper.style.width = "90%";
 	wrapper.style.margin = '0 auto'
 	wrapper.style.border = '2px solid green'
 	document.body.appendChild(wrapper)
 
-	for(let i = 0;i<26;i++)
+	 	// var hintText = document.createElement("div") // this box holds the hint phrase for the word
+	    // hintText.style.width = "auto"
+	    A.style.border = "2px solid blue"
+	    // hintText.className = "hintText"
+	    A.innerHTML = hintphrase
+		// wrapper.appendChild(hintText)
+
+	    createGuessWord(guessWord)
+
+	for(let i = 0;i<26;i++) // this for loop creates the button layout for the alphabet, thats why it only goes to 26 because their are 26 characers in the alphabet
 	    {
 	        var alpha = document.createElement('div')
 	        alpha.style.width = '20px'
@@ -29,15 +45,15 @@ function createletterboxes(word,hintphrase)
 	        var boxes = document.getElementsByClassName('a')
 	        boxes[i].addEventListener('click', function(event)
 	        {
-	            console.log(event.target.innerHTML)
-		            for(i=0;i<word2.length;i++)
+	            // console.log(event.target.innerHTML)
+		            for(i=0;i<word2.length;i++) // this checks to see if a letter on the button layout is pressed is any of the letters in the word you are trying to solve
 		            {	
-			            if(event.target.innerHTML == word2[i])
+			            if(event.target.innerHTML == word2[i]) // if one of the 
 			            {
-			            	console.log("you pressed the letter " + word2[i])
-			            	guessword2 = guessWord2.splice(i, 1, word2[i]); //replacing the guess word with all * with correct letter
-			            	// guessword2 = guessword2.join(""); // i dont think you will need this to change array into string
-			            	console.log(guessWord2)
+			            	
+			            	guessWord2.splice(i, 1, word2[i]); //replacing the * with the correct letter, but leaving all the other letters with *
+			            	var guessWord4 = guessWord2.join("") // join guessword2, so that is goes from from an array into a string
+			            	createGuessWord(guessWord4)
 			            }
 			            else
 			            {
@@ -48,27 +64,21 @@ function createletterboxes(word,hintphrase)
 
 	    }
 
-	    var hintText = document.createElement("div")
-	    hintText.style.width = "100px"
-	    hintText.style.border = "2px solid blue"
-	    hintText.className = "hintText"
-	    hintText.innerHTML = hintphrase
-	    wrapper.appendChild(hintText)
+	    // console.log("The hint for this word is " + hintphrase)
+	    // console.log("Here is the guest word " + guessWord)
+	    // console.log("The original word is " + word )
+	    // console.log(word2)
 
-	    var guessText = document.createElement("div")
-	    guessText.style.width = "50px"
-	    guessText.style.border = "2px solid yellow"
-	    guessText.className = "guessText"
-	    guessText.innerHTML = guessWord
-	    wrapper.appendChild(guessText)
+}
 
 
-	    console.log("The hint for this word is " + hintphrase)
-	    console.log("Here is the guest word " + guessWord)
-	    console.log("The original word is " + word )
-	    console.log(word2)
-
-
+function createGuessWord(guessWord3)
+{
+	guessText.style.width = "auto"
+	guessText.style.border = "2px solid yellow"
+	guessText.className = "guessText"
+	guessText.innerHTML = guessWord3
+	wrapper.appendChild(guessText)
 }
 
 
